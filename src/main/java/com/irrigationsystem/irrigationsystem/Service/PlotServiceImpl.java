3107f226d79f16ea143f80a68230776ca09e34df
Zetaton <Support@zetaton.com>
2023-01-21 18:53:14 +0200

    Recode the task

--- src/main/java/com/irrigationsystem/irrigationsystem/Service/PlotServiceImpl.java
+++ src/main/java/com/irrigationsystem/irrigationsystem/Service/PlotServiceImpl.java
@@ -1,13 +1,6 @@
 package com.irrigationsystem.irrigationsystem.Service;
 
-import com.irrigationsystem.irrigationsystem.Entity.Plot;
-import com.irrigationsystem.irrigationsystem.Entity.TimeSlots;
-import com.irrigationsystem.irrigationsystem.Repository.PlotRepository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PlotServiceImpl implements PlotService{
